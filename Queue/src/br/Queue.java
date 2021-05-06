11ac74812e0205ffff3e9aecd76bc2c664909214
Charith Gimhana <charithgimhana97@gmail.com>
2021-05-06 20:56:18 +0530

    Enqueue method

--- Queue/src/br/Queue.java
+++ Queue/src/br/Queue.java
@@ -1,4 +1,42 @@
 package br;
 
 public class Queue {
+
+    int[] numbers;
+
+    public void enqueue(int number) {
+        if (numbers == null) {
+            numbers = new int[1];
+            numbers[0] = number;
+        } else {
+            int[] temp = new int[numbers.length + 1];
+            for (int i = 0; i < numbers.length; i++) {
+                temp[i] = numbers[i];
+            }
+            temp[temp.length - 1] = number;
+            numbers = temp;
+        }
+    }
+
+
+
+
+
+    public boolean empty() {
+        return numbers == null;
+    }
+
+
+    public void print() {
+        if (empty()) {
+            System.out.println("[]");
+            return;
+        }
+        System.out.print("[");
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.print(numbers[i] + ", ");
+        }
+        System.out.println("\b\b]");
+    }
+
 }
--- Queue/src/br/QueueTest.java
+++ Queue/src/br/QueueTest.java
@@ -1,4 +1,17 @@
 package br;
 
 public class QueueTest {
+    public static void main(String[] args) {
+        Queue queue=new Queue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.print();
+       // queue.pop();
+        queue.print();
+
+    }
+
+
 }
