30fc910ccd638730ed41643284e176a2ad5f60a7
Charith Gimhana <charithgimhana97@gmail.com>
2021-05-07 10:00:31 +0530

    Making the deque class

--- Deque/src/ds/DequeTest.java
+++ Deque/src/ds/DequeTest.java
@@ -1,4 +1,13 @@
 package ds;
 
 public class DequeTest {
+    public static void main(String[] args) {
+        Deque queue=new Deque();
+        queue.deque(10);
+        queue.deque(20);
+        queue.deque(30);
+        queue.deque(40);
+
+
+    }
 }
